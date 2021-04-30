02e922623ab98042ea9912b2c5a3198acf1ad945
Selemmerili <77363152+Selemmerili@users.noreply.github.com>
2021-04-30 14:57:47 +0200

    Create mot_de_passe.py

---
+++ part 2/mot_de_passe.py
@@ -0,0 +1,22 @@
+
+def mot_de_passe(mdp):
+    res = 0
+    res2 = 0
+    res3 = 0
+    special_characters = "!@#$%^&*()-+?_=,<>/"
+
+    if len(mdp) >= 8:
+        for c in mdp:
+            if c in special_characters:
+                res += 1
+            if c.isdigit():
+                res2 += 1
+            if c.isalpha():
+                res3 += 1
+        if res >= 1 and res2 >= 1 and res3 >= 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+      
