fbe79f8024396863b55e0210ea7c96b90f99fdc1
Khalid-34 <77361939+Khalid-34@users.noreply.github.com>
2021-04-30 14:00:33 +0200

    Update rendu_copy.py

--- part 2/rendu_copy.py
+++ part 2/rendu_copy.py
@@ -1,48 +1,12 @@
 
 
-# def isalpha(lettre):
-# 	for i in lettre:
-# 		if i=='azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN':
-# 			retrun true
-
-# 		else:
-# 			return false
-
-# def isdigit('nombre'):
-# 	for i in nombre:
-# 		if i =='1234567890'
-# 			return true
-# 		else:
-# 			return false
-
-
-# def isalnum('alphanumérique'):
-# 	for i in alphanumérique
-# 		if i =='azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890':
-# 			return true
-# 		else:
-# 			return false
-
-# def tolower('lettre'):
-# 	for i in lettre:
-# 		if i =='ABC' 
-
-
-
-# def lencmp("x","y"):
-# 	if "x","y" == "aa", "b"
-# 		return 1
-# 	if "x","y" == "a", "b"
-# 		return 0
-# 	if "x","y" == "a","aa"
-# 		return -1
-
-
-
 def Inverser(x):
-	# return x.reverse()
-	y = x[::-1]
-	return y
+    x = x.split()
+    y=[]
+    for i in x:
+        i = i[::-1]
+        y+=[i]
+    return " ".join(y)
 
 def Fizz_Buzz(y):
 	if y % 3== 0 and y % 5 == 0:
@@ -57,19 +21,5 @@ def Fizz_Buzz(y):
 			
 
 
-# def Multiplication(chiffre):
-# 	for i in chiffre:
-# 		return i*=i
 
-# def Trois_mots('a'):
-# 	if a == 'abc abc abc':
-# 		return true
-# 	else:
-# 		return false
-
-
-
-# print(Fizz_Buzz(30))
-
-print(Inverser('asds22E0ssasq'))
 
