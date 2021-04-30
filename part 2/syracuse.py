91c3345507fb1373e6278b635fec8cccf0b7e7c9
KellianT <77360155+KellianT@users.noreply.github.com>
2021-04-30 14:50:06 +0200

    syracuse.py + tests

---
+++ part 2/syracuse.py
@@ -0,0 +1,23 @@
+def syracuse(n):
+	try:
+		n = int(n)
+		if n < 1:
+			return "Vous n'avez pas saisie une bonne valeur"
+		else:
+			new_str = []
+			while n != 1 :
+				if n%2 == 0:
+					n = n//2
+					new_str += [n]
+				elif n%2 != 0:
+					n = n*3+1
+					new_str += [n]
+		return new_str
+	except ValueError as e:
+		raise(e)
+
+
+
+# print(syracuse(7))
+# print(syracuse(10))
+print(syracuse("b"))
